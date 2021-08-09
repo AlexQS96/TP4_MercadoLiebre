ee952693fb0f6ff4fe187e8698aa7fcb32eb3b6f
alexqs96 <alex.rps96@gmail.com>
2021-08-09 00:29:15 -0300

    parte 6

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"))
 })
 
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+})
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"))
+})
+
 app.listen(port, () => {
     console.log(`Servidor Mercado-Liebre Corriendo en ${port}\ https://localhost:${port}`);
 });
